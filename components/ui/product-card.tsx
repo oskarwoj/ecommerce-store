4613073a452786f4b14b28b454f54fcac794e6ec
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-01 17:21:27 +0200

    Added product-card

---
+++ components/ui/product-card.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Product } from "@/types";
+
+interface ProductCard {
+  data: Product;
+}
+
+const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
+      {/* Image & actions */}
+      <div className="aspect-square rounded-xl bg-gray-100 relative">
+        <Image
+          src={data.images?.[0]?.url}
+          alt=""
+          fill
+          className="aspect-square object-cover rounded-md"
+        />
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+          <div className="flex gap-x-6 justify-center"></div>
+        </div>
+      </div>
+      {/* Description */}
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
+      </div>
+      {/* Price & Review */}
+      <div className="flex items-center justify-between"></div>
+    </div>
+  );
+};
+
+export default ProductCard;
