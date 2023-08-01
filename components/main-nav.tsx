c5386ae814a8ef8f09e05cc1e8650e9ce96f6a31
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-01 12:49:04 +0200

    Fix link warnings

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -23,6 +23,7 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
       {routes.map((route) => (
         <Link
+          rel="preload"
           key={route.href}
           href={route.href}
           className={cn(
