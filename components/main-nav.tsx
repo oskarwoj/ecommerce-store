86d8d32e7b484b86ae9ae6e4cc692ff97311dd76
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-01 09:53:38 +0200

    Created main-nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -3,8 +3,11 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+import { cn } from "@/lib/utils";
+import { Category } from "@/types";
+
 interface MainNavProps {
-  data: any;
+  data: Category[];
 }
 
 const MainNav: React.FC<MainNavProps> = ({ data }) => {
@@ -19,7 +22,14 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
       {routes.map((route) => (
-        <Link key={route.href} href={route.href}>
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors  hover:text-black",
+            route.active ? "text-black" : "text-neutral-500"
+          )}
+        >
           {route.label}
         </Link>
       ))}
