1d0287c4adaac7d101d8ff942ad76036da96a69f
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-01 16:19:23 +0200

    Created dashboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,16 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import getBillboard from "@/actions/getBillboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+const HomePage = async () => {
+  const billboard = await getBillboard("3cb8fd64-1129-46f1-8829-2816d701105c");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export const revalidate = 0;
--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Button from "@/components/ui/button";
 import { ShoppingBag } from "lucide-react";
 import { useEffect, useState } from "react";
