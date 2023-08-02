2e87cd62cacaad01a0fb1351b6c72c58a7f37eae
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-02 17:49:37 +0200

    Product Page

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -12,7 +12,7 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
         style={{ backgroundImage: `url(${data?.imageUrl})` }}
       >
         <div className="h-full w-full flex fle-col justify-center items-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs  ">
+          <div className="font-bold text-white text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
             {data.label}
           </div>
         </div>
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -6,10 +6,11 @@ interface ProductListProps {
   title: string;
   items: Product[];
 }
+
 const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
   return (
     <div className="space-y-4">
-      <h3 className="text-bold text-3xl">{title}</h3>
+      <h3 className="font-bold text-3xl">{title}</h3>
       {items.length === 0 && <NoResults />}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {items.map((item) => (
