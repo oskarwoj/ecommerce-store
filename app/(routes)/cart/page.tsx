12d36044cb89a531e3398fff90a88644b755d85e
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-06 10:14:03 +0200

    Summary added

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,11 +1,26 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import Container from "@/components/ui/container";
 import useCart from "@/hooks/use-cart";
+
 import CartItem from "./components/cart-item";
+import Summary from "./components/summary";
+
+export const revalidate = 0;
 
 const CartPage = () => {
+  const [isMounted, setIsMounted] = useState(false);
   const cart = useCart();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <div className="bg-white">
@@ -15,7 +30,7 @@ const CartPage = () => {
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
               {cart.items.length === 0 && (
-                <p className="text-neutral-500">No items added to cart</p>
+                <p className="text-neutral-500">No items added to cart.</p>
               )}
               <ul>
                 {cart.items.map((item) => (
@@ -23,6 +38,7 @@ const CartPage = () => {
                 ))}
               </ul>
             </div>
+            <Summary />
           </div>
         </div>
       </Container>
