728e20d3622cdb696cf170da613952913ad650c0
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-06 12:48:25 +0200

    Fix summary component

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -2,11 +2,11 @@
 
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
-import { useEffect } from "react";
 import { toast } from "react-hot-toast";
 
 const Summary = () => {
@@ -19,10 +19,11 @@ const Summary = () => {
       toast.success("Payment completed.");
       removeAll();
     }
+
     if (searchParams.get("canceled")) {
       toast.error("Something went wrong.");
     }
-  });
+  }, [searchParams, removeAll]);
 
   const totalPrice = items.reduce((total, item) => {
     return total + Number(item.price);
@@ -35,19 +36,24 @@ const Summary = () => {
         productIds: items.map((item) => item.id),
       }
     );
+
     window.location = response.data.url;
   };
 
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
-      <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
+      <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
           <div className="text-base font-medium text-gray-900">Order total</div>
           <Currency value={totalPrice} />
         </div>
       </div>
-      <Button onClick={onCheckout} className="w-full mt-6">
+      <Button
+        onClick={onCheckout}
+        disabled={items.length === 0}
+        className="w-full mt-6"
+      >
         Checkout
       </Button>
     </div>
