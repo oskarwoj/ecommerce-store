309a6b5430e7f8717a00f4e2e9d64e430bc4706f
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-29 17:03:02 +0200

    Name change

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,19 +7,19 @@ import Container from "@/components/ui/container";
 export const revalidate = 0;
 
 const HomePage = async () => {
-  const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("3cb8fd64-1129-46f1-8829-2816d701105c");
+	const products = await getProducts({ isFeatured: true });
+	const billboard = await getBillboard("8a7240e3-ca45-4f60-89b8-e01ebcd93884");
 
-  return (
-    <Container>
-      <div className="space-y-10 pb-10">
-        <Billboard data={billboard} />
-        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Featured Products" items={products} />
-        </div>
-      </div>
-    </Container>
-  );
+	return (
+		<Container>
+			<div className="space-y-10 pb-10">
+				<Billboard data={billboard} />
+				<div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+					<ProductList title="Featured Products" items={products} />
+				</div>
+			</div>
+		</Container>
+	);
 };
 
 export default HomePage;
